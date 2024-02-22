a2b022670b6a69bdf2990887c4930bf388591f98
Muhammad Umar Anzar <63141196+umar-anzar@users.noreply.github.com>
2024-02-23 00:08:32 +0500

    fix: gpt class fix, schema, system_prompt added in chat function, response_type added

--- docs/LLM/ChatGPT/ChatGPTClass.py
+++ docs/LLM/ChatGPT/ChatGPTClass.py
@@ -1,74 +1,104 @@
-import sys
-from os.path import abspath, dirname
-current_dir = dirname(abspath(__file__))
-parent_dir = dirname(current_dir)
-sys.path.append(parent_dir)
-from config import Config
-
-
-
-import os
 from enum import Enum
 from openai import OpenAI
+from openai.types import Completion
 from pydantic import BaseModel
 
-class RoleEnum(str, Enum):
-    system: str = 'system'
-    user: str = 'user'
-    assistant: str = 'assistant'
+
+class ResponseType(str, Enum):
+    message = 'message'
+    completion_obj = 'completion_obj'
+    message_list = 'message_list'
+
+class Role(str, Enum):
+    system = "system"
+    user = "user"
+    assistant = "assistant"
 
 class MessageSchema(BaseModel):
-    role: RoleEnum
+    role: Role
     content: str
+
+class GPTTokenLimit(Exception):
+    """Exception raised when GPT API response message exceeds token limit."""
+    finish_reason = 'length'
+    def __init__(self, message="GPT API response message exceeds token limit"):
+        self.message = message
+        super().__init__(self.message)
 
 class ChatGPT:
     
     def __init__(self, 
                  api_key: str, 
                  model: str = "gpt-3.5-turbo",
-                 system_prompt: str = ''
+                 system_prompt: str = '',
+                 max_tokens: int | None = None,
+                 temperature: float | None = None
             ) -> None:
-        os.environ['OPENAI_API_KEY'] = api_key
+        
         self.client: OpenAI = OpenAI(api_key=api_key)
         self.model = model
-        self.messages: list[MessageSchema] = [MessageSchema(role=RoleEnum.system.value, content=system_prompt)]
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+        self.token_usage: int = 0
+        self.message_list: list[MessageSchema] = [MessageSchema(role=Role.system, content=system_prompt)]
+
+
+    def _chat(self, message_list: list[MessageSchema]) -> Completion:
+        return self.client.chat.completions.create(
+            model=self.model, 
+            messages=message_list,
+            temperature=self.temperature,
+            max_tokens=self.max_tokens
+            )
 
     def chat(self,
-             message: str, 
-             only_message:bool = True, 
-             history: bool = False
-        ) -> str | dict:
+             message: str | list[MessageSchema] | list[dict],
+             response_type: ResponseType = ResponseType.message,
+             history: bool = False,
+             system_prompt: str | None = None
+        ) -> MessageSchema | list[MessageSchema]:
 
-        messages: list[MessageSchema] | MessageSchema = []
+        if response_type not in ResponseType.__members__:
+            raise ValueError("Invalid response type provided. Must be one of: message, completion_obj, message_list")
 
-        send: MessageSchema = MessageSchema(role=RoleEnum.user.value, content=message)
-        messages = self.messages.copy()
-        messages.append(send)
+        message_list: list[MessageSchema] 
 
-        # response = self.client.chat.completions.create(model=self.model, messages=messages)
-        # reply: MessageSchema = MessageSchema(role=RoleEnum.assistant.value, content=response.choices[0].message.content)
+        if isinstance(message, str):
+            send: MessageSchema = MessageSchema(role=Role.user, content=message)
+            message_list = self.message_list.copy()
+            message_list.append(send)
+        elif isinstance(message, list):
+            try:
+                message_list = [MessageSchema(role=item.role, content=item.content) for item in message]
+            except AttributeError:
+                message_list = [MessageSchema.model_validate(item) for item in message]
+
+               
+
+        if system_prompt is not None:
+            if message_list[0].role == Role.system:
+                message_list[0].content = system_prompt
+
+        response: Completion = self._chat(message_list=message_list)
         
-        response = None
-        reply: MessageSchema = MessageSchema(role=RoleEnum.assistant.value, content=message+message)
+        reply: MessageSchema = MessageSchema(role=Role.assistant, content=response.choices[0].message.content)
+        self.token_usage += response.usage.total_tokens
+
+        if ((self.max_tokens is not None) and (self.token_usage >= self.max_tokens)):
+            raise GPTTokenLimit()
+
+        if response.choices[0].finish_reason == GPTTokenLimit.finish_reason:
+            raise GPTTokenLimit()
+
+        message_list.append(reply)
 
         if history:
-            self.messages.append(send)
-            self.messages.append(reply)
-        
+            self.message_list = message_list.copy()
+            self.message_list.append(reply)
 
-        print(reply)
-        return reply if only_message else response
-
-
-def main():
-    bot = ChatGPT(api_key=Config.openai_api_key)
-    bot.chat('HI', history=True)
-    # bot.chat('b', history=True)
-    # bot.chat('c', history=False)
-    # bot.chat('d', history=True)
-    # bot.chat('e', history=True)
-
-
-
-if __name__ == '__main__':
-    main()
+        if response_type == ResponseType.message:
+            return reply
+        elif response_type == ResponseType.message_list:
+            return message_list
+        elif response_type == ResponseType.completion_obj:
+            return response
