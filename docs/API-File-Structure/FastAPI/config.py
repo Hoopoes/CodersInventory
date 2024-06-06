af697c6c20e158b7d4d7254eee8d66c18ea2d712
umar-anzar <63141196+umar-anzar@users.noreply.github.com>
2024-06-06 15:43:28 +0500

    chore: remove fastapi

--- docs/API-File-Structure/FastAPI/config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-from os import getenv
-
-load_dotenv()
-
-class Config:
-    app_name = "<APP_NAME>"
-    description = "<APP_DESCRIPTION>"
-    api_key = getenv("API_KEY")
--- docs/API-File-Structure/FastAPI/prisma/partial_types.py
+++
@@ -1,3 +0,0 @@
-from prisma.models import Collection
-
-Collection.create_partial(name="CollectionSubFields", include={"id", "user_id", "action"})
